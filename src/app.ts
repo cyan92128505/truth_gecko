f2409aba7045625bdcb4af7bac9b3de41cd40c79
cyan9218505 <cyan92128505@gmail.com>
2022-09-08 17:07:52 +0800

    fix: root route use get method

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ import Database from "./config/database";
 async function APP() {
     const app: Application = express();
     const database = new Database();
-    app.use("/", async (req: Request, res: Response, next: NextFunction) => {
-        const _list = await database.instance.query('SELECT * FROM users');
 
+    app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+        const _list = await database.instance.query('SELECT datname FROM pg_database;');
+
+        console.dir(_list)
 
         res.json(_list);
     });
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,9 +3,6 @@ import { Sequelize } from "sequelize";
 class Database {
     instance: Sequelize;
     constructor() {
-        console.log(`process.env.LOCAL_DB: ${process.env.LOCAL_DB}`);
-        console.log(`process.env.REMOTE_DB: ${process.env.REMOTE_DB}`);
-
         this.instance = new Sequelize(
             `${process.env.ENV === 'dev' ? process.env.LOCAL_DB : process.env.REMOTE_DB}`,
             {
